59998b0b962364144cd3e9e463bfa2fd7c176a5f
Jeong <jjb080319@gmail.com>
2022-06-28 15:25:58 +0900

    sort

--- widget.cpp
+++ widget.cpp
@@ -24,6 +24,39 @@ Widget::~Widget()
 }
 
 
+std::vector<std::string> Widget::split(std::string str, char Delimiter) {
+    std::istringstream iss(str);             // istringstream에 str을 담는다.
+    std::string buffer;                      // 구분자를 기준으로 절삭된 문자열이 담겨지는 버퍼
+
+    std::vector<std::string> result;
+
+    // istringstream은 istream을 상속받으므로 getline을 사용할 수 있다.
+    while (getline(iss, buffer, Delimiter)) {
+        result.push_back(buffer);               // 절삭된 문자열을 vector에 저장
+    }
+
+    return result;
+}
+
+std::vector<std::string> Widget::sort(std::vector<std::string>str)
+{
+    int len = str.size();
+
+    std::string temp;
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = 0; j < len - i - 1; j++)
+        {
+            if (str[j].compare(str[j + 1]) > 0)
+            {
+                temp = str[j];
+                str[j] = str[j + 1];
+                str[j + 1] = temp;
+            }
+        }
+    }
+    return str;
+}
 
 void Widget::show_time()
 {
@@ -38,7 +71,7 @@ void Widget::on_btn_clicked()//버튼 클릭시
     QString time = slt_time.toString("hh시 mm분");
     QString str = time + ui->date_input->text();
     std::string send_str = "update/" + str.toStdString();
-    write(sock,send_str.c_str(),sizeof(send_str));
+    write(sock,send_str.c_str(),sizeof(send_str.c_str()));
     ui->text->append(str);
 }
 
@@ -46,7 +79,7 @@ void Widget::on_calendar_clicked()
 {
     char msg[1024];
     QDate slect_date = ui->calendar->selectedDate();//selectedDate의 반환값은 QDate이기에 같은 QDate형태에 저장
-    slt_date = slect_date.toString();
+    slt_date = slect_date.toString("hh시 mm분");
     ui->text->clear();
     ui->date->setDate(slect_date);
     std::string str = "date/" + slt_date.toStdString();
@@ -54,22 +87,10 @@ void Widget::on_calendar_clicked()
     read(sock,msg,sizeof(msg));
     std::string split_msg(msg);
     std::vector<std::string> recv_msg = Widget::split(split_msg,'/');
+    recv_msg = Widget::sort(recv_msg);
     if(strcmp(msg,"X")==-1)
         for(auto st: recv_msg)
         {
             ui->text->append(QString::fromStdString(st));
         }
-}
-std::vector<std::string> Widget::split(std::string str, char Delimiter) {
-    std::istringstream iss(str);             // istringstream에 str을 담는다.
-    std::string buffer;                      // 구분자를 기준으로 절삭된 문자열이 담겨지는 버퍼
-
-    std::vector<std::string> result;
-
-    // istringstream은 istream을 상속받으므로 getline을 사용할 수 있다.
-    while (getline(iss, buffer, Delimiter)) {
-        result.push_back(buffer);               // 절삭된 문자열을 vector에 저장
-    }
-
-    return result;
 }
